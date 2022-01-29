146b8f7f4d98dbec135e08442c0abf6a15aef0ac
jromera <romera.gomez.jorge@gmail.com>
2022-01-29 19:09:00 -0300

    Create test to locationCostController

---
+++ app/tests/Entity/LocationCostsMother.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\LocationCosts;
+use App\Tests\Entity\Mother\IntegerMother;
+use App\Tests\Entity\Mother\WordMother;
+
+class LocationCostsMother
+{
+    public static function create(string $location, int $cost ): LocationCosts
+    {
+    	$locationCosts = new LocationCosts();
+	
+	    $locationCosts->setLocation($location);
+	    
+	    $locationCosts->setCost((string) $cost);
+    	
+        return  $locationCosts;
+    }
+
+    public static function random(): LocationCosts
+    {
+        return self::create( WordMother::random(),IntegerMother::lessThan(100000));
+    }
+    
+}
